5f33da763b353586ca4f69a7f29ae938a478f866
Ali Adelkhah <sali.adelkhah@yahoo.com>
2020-09-29 19:59:54 +0330

    code infix to postfix

--- DataStructure2.cpp
+++ DataStructure2.cpp
@@ -1,5 +1,9 @@
 //data strcture exercise 2
 
+#include<bits/stdc++.h>
+using namespace std;
+
+
 bool is_operator(string s)
 {
 	if(s == "*" || s == "+" || s == "/" || s == "^" || s == "-"){
@@ -15,7 +19,7 @@ bool check_valid(string a[], int n)
 		return false;
 	}
 	for(int i = 1; i < n; i++){
-		if(is_operator(a[i-1]) || is_operator(a[i])){
+		if(is_operator(a[i-1]) && is_operator(a[i])){
 			return false;
 		}
 	}
@@ -37,46 +41,95 @@ bool check_valid(string a[], int n)
 
 }
 
-string* infix_to_postfix(string infix[], int n)
+void infix_to_postfix(string a[], int n, string postfix[])
 {
-	string postfix[n];
-
+	if(!check_valid(a, n)){
+		return;
+	}
+	int id = 0; // for postfix
+	stack<string> stk;
 
 	for(int i = 0; i < n; i++){
 		if(a[i] == "("){
-
+			stk.push(a[i]);
 		}
 		else if(a[i] == ")"){
-
+			while(!stk.empty() && stk.top() != "("){
+				postfix[id] = stk.top();
+				id++;
+				stk.pop();
+			}
+			stk.pop(); // popping the "(";
 		}
 		else if(a[i] == "+"){
-
+			while(!stk.empty() && stk.top() != "("){
+				postfix[id] = stk.top();
+				id++;
+				stk.pop();
+			}
+			stk.push(a[i]);
 		}
 		else if(a[i] == "-"){
-
+			while(!stk.empty() && stk.top() != "("){
+				postfix[id] = stk.top();
+				id++;
+				stk.pop();
+			}
+			stk.push(a[i]);
 		}
 		else if(a[i] == "*"){
-
+			while(!stk.empty() && stk.top() != "(" && stk.top() != "+" && stk.top() != "-"){
+				postfix[id] = stk.top();
+				id++;
+				stk.pop();
+			}
+			stk.push(a[i]);
 		}
 		else if(a[i] == "/"){
-
+			while(!stk.empty() && stk.top() != "(" && stk.top() != "+" && stk.top() != "-"){
+				postfix[id] = stk.top();
+				id++;
+				stk.pop();
+			}
+			stk.push(a[i]);
 		}
 		else if(a[i] == "^"){
-
+			stk.push(a[i]);
 		}
 		else{ // a[i] is a number or variable
-			postfix.push(a[i]);
+			postfix[id] = a[i];
+			id++;
 		}
+	}
+	while(!stk.empty()){
+		postfix[id] = stk.top();
+		id++;
+		stk.pop();
 	}
 }
 
 
+
 int postfix_ecalation(string postfix[], int n)
+{
+	return 0;
+}
+
+void infix_to_prefix(string infix[], int n, string prefix)
 {
 
 }
 
-string* infix_to_prefix(string infix[], int n)
-{
 
+int main()
+{
+	
+	string a[] = {"a","^","b","*","c","*","d","-","(","(","e","+","f","/","g",")","-","h",")"};
+	int n = 19;
+	string ans[n];
+	infix_to_postfix(a, n, ans);
+	for(int i = 0; i < n; i++){
+		cout << ans[i];
+	}
+	return 0;
 }
