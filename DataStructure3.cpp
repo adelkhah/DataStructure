025e16e9e2ea792168c27a02eb5d02783e035df4
Ali Adelkhah <sali.adelkhah@yahoo.com>
2020-10-28 14:55:59 +0330

    solve double ended circular queue

--- DataStructure3.cpp
+++ DataStructure3.cpp
@@ -1,75 +1,138 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX_SIZE = 10000;
-
-class PriorityQueue
+class DoubleEndedCircular
 {
-	int size = 0;
-	int a[MAX_SIZE];
-private:
-	int find_highest_priority(){ // returns the index of that element
-		int index;
-		int maximum = 0;
-		for(int i = 0; i < size; i++){
-			if(a[i] > maximum){
-				maximum = a[i];
-				index = i;
-			}
-		}
-		return index;
-	}
+	int front = 0;
+	int last = 1;
+	int size; // size of the queue 
+	int cnt; // the number of elements in the queue
+	int *a; // array
+
 public:
-	void insert(int x){
-		a[size] = x;
-		size++;
-	}
-	void remove(){
-		int index = find_highest_priority();
-		for(int i = index; i < size; i++){ // shift [index,size) to left
-			a[i] = a[i+1];
+
+	/// not a main function
+	void show(){ // just to have a quick look at the queue not
+		for(int i = (front+1)%size; i != last; i++,i %= size){
+			cout << i << "  " << a[i] << endl;
 		}
-		size--;
+		return;
+	}
+	/// not a main function
+
+
+	DoubleEndedCircular(int k){
+		a = new int[k]; // creat array of size k
+		size = k;
 	}
 
-	bool empty(){
-		if(size == 0){
+	bool insertFront(int x){
+		if(isFull()){
+			return false;
+		}
+		a[front] = x;
+		front--;
+		// just in case that front gets equel to -1
+		front += size; // you can also check it by if(front==-1)
+		front %= size;
+		cnt++;
+		return true;
+	}
+
+	bool insertLast(int x){
+		if(isFull()){
+			return false;
+		}
+		a[last] = x;
+		last++;
+		last %= size;
+		cnt++;
+		return true;
+	}
+
+	bool deleteFront(){
+		if(isEmpty()){
+			return false;
+		}
+		front++;
+		front %= size;
+		cnt--;
+		return true;
+	}
+
+	bool deleteLast(){
+		if(isEmpty()){
+			return false;
+		}
+		last--;
+		// just in case that last gets equel to -1
+		last += size; // you can also check it by if(front==-1)
+		last %= size;
+		cnt--;
+		return true;
+	}
+
+	int getFront(){
+		if(isEmpty()){
+			return -1;
+		}
+		int f = front;
+		f++;
+		f %= size;
+		return a[f];
+	}
+
+	int getLast(){
+		if(isEmpty()){
+			return -1;
+		}
+		int l = last;
+		l--;
+		l += size;
+		l %= size;
+		return a[l];
+	}
+
+	bool isEmpty(){
+		if(cnt == 0){
 			return true;
 		}
 		return false;
 	}
 
-};
-
-class Deque // implementing input-restricted which insertion is one sided
-{
-	int size = 0;
-	int a[MAX_SIZE];
-public:
-	void remove_back(){
-		size--;
-	}
-	void remove_front(){
-		for(int i = 0; i < size; i++){ // shift [1,size) to left
-			a[i] = a[i+1];
-		}
-		size--;
-	}
-	void insert(int x){
-		a[size] = x;
-		size++;
-	}
-	bool empty(){
-		if(size == 0){
+	bool isFull(){
+		if(cnt == size){
 			return true;
 		}
 		return false;
+	}
+
+	~DoubleEndedCircular(){
+		delete[] a; // delete the array otherwise leak memory happens
 	}
 	
 };
 
+
+
 int main()
 {
-	// no need to test :)
+	DoubleEndedCircular q(10);
+	q.insertLast(10);
+	q.insertLast(3);
+	q.insertLast(123);
+	q.insertFront(4);
+	q.insertFront(6);
+	q.insertFront(444);
+	q.show();
+	cout << q.getFront() << endl;
+	cout << q.getLast() << endl;
+	q.deleteLast();
+	q.deleteLast();
+	q.deleteLast();
+	q.deleteLast();
+	cout << q.getLast() << endl;
+	q.show();
+	// we can see that every thing is fine :)
 	return 0;
 }
